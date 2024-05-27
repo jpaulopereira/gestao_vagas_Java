b92518323e22a52840dad4834c419e7bb24dab16
jpaulopereira <joaopaulo.ae@gmail.com>
2024-05-27 12:49:29 -0300

    adicao dto

---
+++ src/main/java/br/com/jotape/gestaovagas/dto/AuthCompanyResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.jotape.gestaovagas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCompanyResponseDTO {
+
+    private String acess_token;
+    private String expires_in;
+}
---
+++ src/main/java/br/com/jotape/gestaovagas/dto/CandidateRequestDTO.java
@@ -0,0 +1,7 @@
+package br.com.jotape.gestaovagas.dto;
+
+public record CandidateRequestDTO(
+        String username,
+        String password
+) {
+}
